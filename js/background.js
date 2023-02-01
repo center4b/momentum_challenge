a493e016cc9223a7ffbbdd7d6840773a1def5ff8
Jeong Sangwoo <center_4b@icloud.com>
2023-02-01 18:03:39 +0900

    230201

--- js/background.js
+++ js/background.js
@@ -1,9 +1,10 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+const images = ['0.jpg', '1.jpg', '2.jpg'];
 
 const choosenImage = images[Math.floor(Math.random() * images.length)];
+const backgound = document.querySelector('body');
+backgound.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(img/${choosenImage})`;
 
-const bgImage = document.createElement("img");
+// const bgImage = document.createElement('img');
 
-bgImage.src = `img/${choosenImage}`;
-
-document.body.appendChild(bgImage);
+// bgImage.src = `img/${choosenImage}`;
+// document.body.appendChild(bgImage);
